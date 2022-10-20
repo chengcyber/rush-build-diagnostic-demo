a7f235df0f1c92f9184e8a73298884505c3c9c3f
Cheng Liu <chengcyber@users.noreply.github.com>
2022-10-20 09:50:05 +0800

    chore

--- apps/external-file-access/scripts/build.js
+++ apps/external-file-access/scripts/build.js
@@ -2,10 +2,15 @@ const path = require("path");
 const { execSync } = require("child_process");
 const { JsonFile } = require("@rushstack/node-core-library");
 
-// !!! Access a file outside of the project directory
+// !!! Read a file outside of the project directory
 const outsideJsonFile = path.resolve(__dirname, "../../../outside.json");
 
 const { outside } = JsonFile.load(outsideJsonFile);
 console.log(`read outside: ${outside}`);
 
 execSync(`tsc`);
+
+// !!! Write a file outside of the project directory
+const outsideLogFile = path.resolve(__dirname, "../../../outside.log");
+JsonFile.save({ timestamp: String(Date.now()) }, outsideLogFile);
+console.log(`wrote to outside.log`);
